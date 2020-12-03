dd0580010b55dd78e269ae7e1046ff2e4ba2d8ec
sparkzy <31968082+sparkzy@users.noreply.github.com>
2020-12-02 21:12:51 -0500

    removed lingering lines of unecessary code from testing cloud onfig stuff.

--- src/main/java/gg/sparkzy/casl/userservice/controllers/UserController.java
+++ src/main/java/gg/sparkzy/casl/userservice/controllers/UserController.java
@@ -4,10 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,15 +21,10 @@ import gg.sparkzy.casl.userservice.services.UserService;
 @RequestMapping("/users/")
 @RefreshScope // actuator/refresh
 @CrossOrigin(origins = "http://localhost:4200")
-@EnableAutoConfiguration
-@Configuration
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
-	
-	@Value("${my.greeting}")
-	private String greeting;
 	
 	/************************************************************************************
 	 * Create
